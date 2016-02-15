ade0c348b396ec7107690f6e603695754bdcd35b
picwell-allie <allie@picwell.com>
2016-02-14 20:40:47 -0500

    using mostly the highlighting example now

--- theySaidWhat/popup.js
+++ theySaidWhat/popup.js
@@ -1,20 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+document.addEventListener("DOMContentLoaded", function() {
+	loadOptions();
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+	document.getElementById("buttonCancel").addEventListener("click", function() {
+		window.close();
+	});
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+	document.getElementById("buttonSave").addEventListener("click", function() {
+		saveOptions();
+		window.close();
+
+		chrome.runtime.sendMessage({
+			"message": "getOptions",
+			"remove": true
+		});
+	});
+});
