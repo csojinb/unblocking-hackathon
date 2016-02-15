a01349a1d5f8fb8ab68fd7e8e2be60cd0179a81a
Aditya Natraj <aditya@picwell.com>
2016-02-15 14:02:13 -0500

    adityas stuff

---
+++ theySaidWhat/js/background.js
@@ -0,0 +1,33 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if ("getOptions" == request.message) {
+    if ("undefined" != typeof localStorage) {
+      chrome.tabs.query({
+          "active": true,
+          "currentWindow": true
+        },
+        function(tabs) {
+          if ("undefined" != typeof tabs[0].id && tabs[0].id) {
+            var highlightGender = localStorage.getItem("highlightGender");
+            highlightGender = "true" == highlightGender || null == highlightGender;
+
+            chrome.tabs.sendMessage(tabs[0].id, {
+              "message": "returnOptions",
+              "remove": request.remove,
+              "keywords": localStorage.getItem("keywords"),
+              "foreground": localStorage.getItem("foreground") || "#000000",
+              "background": localStorage.getItem("background") || "#ffff00",
+              "highlightGender": highlightGender
+            });
+          }
+        }
+      );
+    }
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if ("highlightGender" == request.message) {
+    var highlightGender = localStorage.getItem("highlightGender");
+    highlightGender = "true" == highlightGender || null == highlightGender;
+  }
+});
